efc9a89f88ed79b322298f13c43399dd8ffd203e
payal agrawal <pagrawal312@gmail.com>
2017-04-18 00:32:14 -0700

    added link for login and sign up page

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router';
 import './App.css';
 
 class App extends Component {
@@ -8,8 +9,8 @@ class App extends Component {
         <div className="App-header">
           <h2 className="appLogo">Gullak</h2>
           <ul className="headerList">
-            <li>Login</li>
-            <li>Signup</li>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/signup">Signup</Link></li>
           </ul>
           <hr className="lineDivider"/>
         </div>

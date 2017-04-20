aecca84f84f5b3171c100d72a19fbd7da310f291
payal agrawal <pagrawal312@gmail.com>
2017-04-20 01:17:13 -0700

    added actaul text in landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -14,18 +14,20 @@ class LandingPage extends Component {
             <div className="col picAlign">
             </div>
             <div className="col About">
-            <p>Allowance, chores and rewards for kids.Helping kids becoming more responsible for money.
-            </p>
-            <p>Making chores fun and help to improve family time by dividing responsibilities.
-            </p>
+              <h3>Assign </h3>
+              <p>Make your kids more responsible by assigning them chores.</p>
+              <p>Pick from a variety of chore categories like household, personal etc. </p>
+              <p>Repeat assignment for multiple days in a week.</p>
+              <p>Negative chores serve as check on the kids and actually deduct allownace upon completion.</p>
+
           </div>
           </section>
           <section className="row aboutAlign">
             <div className="col About">
-            <p>Allowance, chores and rewards for kids.Helping kids becoming more responsible for money.
-            </p>
-            <p>Making chores fun and help to improve family time by dividing responsibilities.
-            </p>
+              <h3>Track </h3>
+            <p>Keep track of the chores on daily/hourly basis.</p>
+            <p>Approve or reject completion of the chores. </p>
+            <p>Regular traction keeps kids' interest up and makes it fun and interesting.</p>
           </div>
             <div className="col picAlign1">
             </div>
@@ -34,13 +36,14 @@ class LandingPage extends Component {
             <div className="col picAlign2">
             </div>
             <div className="col About">
-            <p>Allowance, chores and rewards for kids.Helping kids becoming more responsible for money.
-            </p>
-            <p>Making chores fun and help to improve family time by dividing responsibilities.
-            </p>
+              <h3>Reward </h3>
+            <p>Instant allowance allocation upon approval of chores completion.</p>
+            <p>Standard allowance associated with each chore.</p>
+            <p>Bonus allowance allocated for extra chores picked by kids. </p>
+            <p>Negative allowance for negative chores.</p>
           </div>
           </section>
-        
+
     </div>
     );
   }
